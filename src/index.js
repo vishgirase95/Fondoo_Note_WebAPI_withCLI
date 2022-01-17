cba9686f35efcf06b7428afcd14233d7ff9860f3
vish <vishgirase95@gmail.com>
2022-01-17 23:28:16 +0530

    [vishal] added swagger installation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
